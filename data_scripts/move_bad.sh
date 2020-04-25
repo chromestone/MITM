e186118871488334ed230bddff72d5d6b0541c57
Derek Zhang <dzhang21@umd.edu>
2020-04-25 17:19:34 -0400

    Create move_bad.sh
    this will move bad gz files out
    part of cleaning up old gz files

---
+++ data_scripts/move_bad.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+let "total=0"
+let "fail=0"
+
+for file in "$@"
+do
+    if ! zcat $file > /dev/null; then
+        let "fail++"
+        mv $file ./bad
+    fi
+    let "total++"
+done
+
+echo "Total is ${total}"
+echo "Fail is ${fail}"
